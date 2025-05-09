328b42f55c44255272cd2f6cccc61454138c5c4d
André Alan Alves <andrealansp@hotmail.com>
2025-05-09 14:32:45 -0300

    Features: Criação de links para as câmeras, tpnet; Adicionada pesquisa por câmeras e quantidade de câmeras

--- cameras/views.py
+++ cameras/views.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from typing import Any
 from django.db.models.query import QuerySet
+from django.db.models import Q
 from django.views.generic.list import ListView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -12,21 +13,33 @@ from .forms import CameraForm
 class CameraListView(LoginRequiredMixin, ListView):
     login_url = "/accounts/login/"
     model = Cameras
-    paginate_by = 15
-    template_name = "camera_list.htnl"
+    paginate_by = 50
+    template_name = "camera_list"
 
     def get_queryset(self) -> QuerySet[Any]:
-        cameras = super().get_queryset().order_by("ponto")
+        cameras = super().get_queryset().order_by("ponto","porta")
         ponto = self.request.GET.get("ponto")
         serial = self.request.GET.get("serial")
         regiao = self.request.GET.get("regiao")
+        nome_camera = self.request.GET.get("nome_camera")
+
         if ponto:
             cameras = cameras.filter(ponto__icontains=ponto)
         if serial:
             cameras = cameras.filter(serial__icontains=serial)
         if regiao:
             cameras = cameras.filter(regiao__icontains=regiao)
+        if nome_camera:
+            cameras = cameras.filter(nome_camera__icontains=nome_camera)
+
         return cameras
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        cameras = self.get_queryset()
+        context["qtd_cameras"] = cameras.count()
+        return context
+
 
 
 class CameraCreateView(LoginRequiredMixin, CreateView):
